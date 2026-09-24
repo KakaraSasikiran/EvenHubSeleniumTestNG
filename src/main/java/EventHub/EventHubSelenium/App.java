b4a8d1868fb993733715220f74c03144a4ba9697
KakaraSasikiran <sasikirankakara8@gmail.com>
2026-09-24 15:38:10 +0530

    commit

---
+++ src/main/java/EventHub/EventHubSelenium/App.java
@@ -0,0 +1,10 @@
+package EventHub.EventHubSelenium;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
